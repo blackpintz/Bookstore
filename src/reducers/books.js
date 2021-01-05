6ecb61f9156dbaf96cc557460f640783f4662370
Bluette1 <marylene.sawyer@gmail.com>
2021-01-05 17:32:47 +0200

    Add `books` reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,3 +1,23 @@
-const booksReducer = () => 'define book actions';
+import { REMOVE_BOOK, CREATE_BOOK } from '../actions/actionTypes';
+
+const findAndDeleteBook = (books, id) => books.filter(book => book.id !== id);
+
+const booksReducer = (state = [], action) => {
+  switch (action.type) {
+    case CREATE_BOOK: {
+      return [
+        ...state,
+        action.book,
+      ];
+    }
+    case REMOVE_BOOK: {
+      const { book } = action.book;
+      const books = [...state];
+      return findAndDeleteBook(books, book.id);
+    }
+    default:
+      return state;
+  }
+};
 
 export default booksReducer;
