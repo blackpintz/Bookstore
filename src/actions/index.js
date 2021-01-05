7f3196f486d99053fb9e5434851f63a2d8a35ddc
Bluette1 <marylene.sawyer@gmail.com>
2021-01-05 17:22:18 +0200

    Add Book form, and book actions: createBook and removeBook
    - Create and connect the redux store

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import { REMOVE_BOOK, CREATE_BOOK } from './actionTypes';
+
+function getRandomIntInclusive(lower, upper) {
+  const min = Math.ceil(lower);
+  const max = Math.floor(upper);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const createBook = book => ({
+  type: CREATE_BOOK,
+  book: {
+    id: getRandomIntInclusive(1, 100),
+    ...book
+  },
+});
+
+export const removeBook = book => ({
+  type: REMOVE_BOOK,
+  book,
+});
