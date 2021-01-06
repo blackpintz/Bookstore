b3697e833a39da32ea03145eccae0040c7a01eb8
Bluette1 <marylene.sawyer@gmail.com>
2021-01-06 05:43:18 +0200

    Move `getRandomIntInclusive` function from actions to books reducer
    - Use ES6 syntax

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,8 @@
 import { REMOVE_BOOK, CREATE_BOOK } from './actionTypes';
 
-function getRandomIntInclusive(lower, upper) {
-  const min = Math.ceil(lower);
-  const max = Math.floor(upper);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 export const createBook = book => ({
   type: CREATE_BOOK,
-  book: {
-    id: getRandomIntInclusive(1, 100).toString(),
-    ...book,
-  },
+  book,
 });
 
 export const removeBook = book => ({
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,13 +1,21 @@
 import { REMOVE_BOOK, CREATE_BOOK } from '../actions/actionTypes';
 
+const getRandomIntInclusive = (lower, upper) => {
+  const min = Math.ceil(lower);
+  const max = Math.floor(upper);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 const findAndDeleteBook = (books, id) => books.filter(book => book.id !== id);
 
 const booksReducer = (state = [], action) => {
   switch (action.type) {
     case CREATE_BOOK: {
+      const { book } = action;
+      book.id = getRandomIntInclusive(1, 100).toString();
       return [
         ...state,
-        action.book,
+        book,
       ];
     }
     case REMOVE_BOOK: {
