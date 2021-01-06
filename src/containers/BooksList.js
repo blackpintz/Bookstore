da7771a80d52f2d67f7cbd40412d7c622b694d46
Bluette1 <marylene.sawyer@gmail.com>
2021-01-06 12:41:38 +0200

    Update `handleRemoveBook`

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -4,37 +4,23 @@ import PropTypes from 'prop-types';
 import Book from '../components/Book';
 import { removeBook } from '../actions/index';
 
-// class BooksList extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-
-//   }
-// }
-const BooksList = ({ books, dispatch }) => {
-  const handleRemoveBook = book => {
-    dispatch(removeBook(book));
-  };
-  return (
-    <div>
-      <p>Table with colgroup</p>
-      <table>
-        <colgroup span="4" />
-        <tbody>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Action</th>
-          </tr>
-          {books.map(book => <Book key={book.id} book={book} handleRemoveBook={handleRemoveBook} />)}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+const BooksList = ({ books, removeBook }) => (
+  <div>
+    <p>Table with colgroup</p>
+    <table>
+      <colgroup span="4" />
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Category</th>
+          <th>Action</th>
+        </tr>
+        {books.map(book => <Book key={book.id} book={book} handleRemoveBook={removeBook} />)}
+      </tbody>
+    </table>
+  </div>
+);
 
 const mapStateToProps = state => ({
   books: state.books,
@@ -42,11 +28,11 @@ const mapStateToProps = state => ({
 
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.object),
-  dispatch: PropTypes.func.isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 BooksList.defaultProps = {
   books: 'no value',
 };
 
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, { removeBook })(BooksList);
